156261bc57d22e50688c1ae4a30cc509ad244426
farookie007 <faroukadekunle001@gmail.com>
2022-10-14 09:54:44 +0000

    Initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,5 +6,8 @@
  */
 int main(void)
 {
+	char err_str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, err_str, 59);
 	return (1);
 }
