05cfe980c7a0053d811f03c9975243cffb6863bc
farookie007 <faroukadekunle001@gmail.com>
2022-10-15 17:35:24 +0000

    Initial commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	int i;
+	char hex_values[] = "0123456789abcdef";
 
-	for (i = 0; i <= 16; i++)
-		putchar(i);
-	printf("\n");
+	for (i = 0; i < 16; i++)
+		putchar(hex_values[i]);
+	putchar('\n');
 	return (0);
 }
