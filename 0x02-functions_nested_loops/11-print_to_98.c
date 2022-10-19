903d3e4d5c3f538d2000ef0aa732e288b76aef46
farookie007 <faroukadekunle001@gmail.com>
2022-10-19 19:54:28 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural number to 98
+ * @n: the number to be counted from
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+		return;
+	}
+	for (n; n <= 98; n++)
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	for(n; n >= 98; n--)
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	_putchar('\n');
+}
