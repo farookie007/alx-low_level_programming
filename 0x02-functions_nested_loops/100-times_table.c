3a2943a6883fb510905c95ebe594801bafd1ca06
farookie007 <faroukadekunle001@gmail.com>
2022-10-19 22:08:33 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,75 @@
+#include "main.h"
+/**
+ * print_times_table - prints the `n` times table
+ * @n: the number to print to
+ *
+ * Return: the table
+ */
+void print_times_table(int n)
+{
+	int x, y, mul;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			_pnum(x * y);
+
+			if (y < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			} else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
+
+/**
+ * _pnum - Prints a number
+ * @n: the number
+ *
+ * Return: the number printed as characters
+ */
+void  _pnum(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = _abs(n);
+	}
+	if (n == 0)
+		_putchar('0');
+
+	if (n > 99 && n < 1000)
+	{
+		_putchar('0' + n / 100);
+		_putchar('0' + (n % 100) / 10);
+		_putchar('0' + n % 10);
+	} else if (n > 9 && n < 100)
+	{
+		_putchar(' ');
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	} else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + n % 10);
+	}
+}
+
+/**
+ * _abs - Return the absolute value of an integer
+ * @n: parameter to be evaluated
+ *
+ * Return: a +ve integer value always
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int add(int, int);
 int add(int, int);
 void print_to_98(int n);
 void _pnum(int);
+void print_times_table(int n);
