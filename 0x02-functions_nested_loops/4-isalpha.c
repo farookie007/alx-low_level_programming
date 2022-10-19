003196fcc94b707d7ff632e5cc57c41249809626
farookie007 <faroukadekunle001@gmail.com>
2022-10-19 12:21:19 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+/**
+ * _isalpha - Function to check if the parameter is an alphabet
+ * @c: parameter checked to confirm it is an alphabet
+ *
+ * Return: 1 if c is an alphabet and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'));
+}
