6aa3248a55f7d3131a883d552717925d233fcd4c
farookie007 <faroukadekunle001@gmail.com>
2022-10-19 13:54:19 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of the parameter
+ * @n: the parameter to be evaluated
+ *
+ * Return: the last digit of `n`
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
