8bad013c9e6dbb8a7ac4f09b72966e770f7e7519
farookie007 <faroukadekunle001@gmail.com>
2022-10-19 14:42:30 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + hr / 10);
+			_putchar('0' + hr % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+		}
+	}
