58adf47a05c9f7555240e0af59f2736c23a5e85d
farookie007 <faroukadekunle001@gmail.com>
2022-10-20 22:50:24 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+void fib(int);
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	fib(100);
+
+	return (0);
+}
+
+/**
+ * fib - Prints certain amount of the fibonacci sequence
+ * @n: the number of fibonacci sequence to be generated
+ *
+ * Return: void
+ */
+void fib(int n)
+{
+	
+}
