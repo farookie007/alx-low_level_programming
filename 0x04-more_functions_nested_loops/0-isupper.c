448778d42d4caf20cd5877409b74b6af1d98ef95
farookie007 <faroukadekunle001@gmail.com>
2022-10-21 00:39:52 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - Program to check if a character is in lower case or not
+ * @c: the parameter to be checked in the function
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
