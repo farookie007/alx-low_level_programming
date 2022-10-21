884f6c5b23e2f2dbe625a748c473b7256f1add10
farookie007 <faroukadekunle001@gmail.com>
2022-10-21 23:37:49 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 0; y <= 10; y++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			
