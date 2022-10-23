0a46980b483509f5dc337d222edaa3b39390d7a0
farookie007 <faroukadekunle001@gmail.com>
2022-10-23 23:29:45 +0000

    Initial commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -27,12 +27,12 @@ void fib(int n)
 	long int val;
 	long int values[2] = {1, 2};
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		if (i == 0)
+		if (i == 1)
 		{
 			printf("%ld", values[0]);
-		} else if (i == 1)
+		} else if (i == 2)
 		{
 			printf("%ld", values[1]);
 		}
