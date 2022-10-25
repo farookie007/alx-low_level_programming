692390446d5a1deaec03a37cee2bec4238609446
farookie007 <faroukadekunle001@gmail.com>
2022-10-25 19:10:08 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_rev - prints a string in reverse followed by a neewline
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		write(1, s[i], 1);
+		i--;
+	}
+	write(1, "\n", 1);
+}
