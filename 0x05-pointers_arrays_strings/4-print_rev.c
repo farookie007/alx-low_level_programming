c72523dac485d5b68f99e73daf8b431d2f79d3d5
farookie007 <faroukadekunle001@gmail.com>
2022-10-25 20:12:10 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,47 @@
  */
 void print_rev(char *s)
 {
-	int i = _strlen(s) - 1;
+	char new_str[] = _revstr(*s);
+	int len = _strlen(*s);
 
-	while (i >= 0)
-	{
-		char arr[2] = {s[i], '\0'};
-		write(1, arr, 1);
-		i--;
-	}
+	write(1, new_str, len);
 	write(1, "\n", 1);
+}
+
+/**
+ * _revstr - reverses a string and returns it
+ * @s: the string to be reversed
+ *
+ * Return: char
+ */
+char _revstr(*s)
+{
+	int len = _strlen(*s);
+	char arr[len];
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		arr[i] = s[len - 1 - i];
+	}
+	arr[len] = '\0';
+	return (arr);
+}
+
+/**
+ * _strlen - Returns the length of the string argument
+ * @s: pointer to the string address
+ *
+ * Return: the length as an integer
+ */
+int _strlen(char *s)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		counter++;
+	}
+	return (counter);
 }
