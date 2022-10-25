aee326284d82d600d47870dd7aa620d3fb24b7e0
farookie007 <faroukadekunle001@gmail.com>
2022-10-25 18:53:26 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,5 +9,24 @@
  */
 void _puts(char *str)
 {
-	write(2, str);
+	write(2, str, _strlen(str));
+}
+
+
+/**
+ * _strlen - Returns the length of the string argument
+ * @s: pointer to the string address
+ *
+ * Return: the length as an integer
+ */
+int _strlen(char *s)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		counter++;
+	}
+	return (counter);
 }
