38b021e3bd03d5ee9194a37e6688466cb5648857
farookie007 <faroukadekunle001@gmail.com>
2022-10-27 23:40:07 +0000

    Initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int len = 0;
 
-	while(dest[i++])
+	while (dest[i++])
 		len++;
 	for (i = 0; src[i] && i < n; i++)
 		dest[len++] = src[i];
