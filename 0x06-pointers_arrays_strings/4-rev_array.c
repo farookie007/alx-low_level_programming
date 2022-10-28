1822f22eda7ba062c0c9fc67d1b2581650162d6e
farookie007 <faroukadekunle001@gmail.com>
2022-10-28 21:20:24 +0000

    Initial commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: the argument
+ * @n: number of elements
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int tempo, i;
+
+	for (i = n - 1; i >= n / 2; index--)
+	{
+
