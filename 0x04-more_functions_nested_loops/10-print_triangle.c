b13c67bb692a0938c4f9e027061fb566df279cf1
farookie007 <faroukadekunle001@gmail.com>
2022-10-30 17:45:02 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,9 +18,23 @@ void print_triangle(int size)
 			_putchar(' ');
 			j++;
 		}
-		_putchar('#');
+		while (j >= i && j <= size)
+		{
+			_putchar('#');
+			j++;
+		}
 		_putchar('\n');
 	}
 	if (size <= 0)
 		_putchar('\n');
+}
+
+void print_triangle(int size)
+{
+	int i;
+
+	for (i = 1, j = size; i <= size; i++, j--)
+	{
+
+	}
 }
