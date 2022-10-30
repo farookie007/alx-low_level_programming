09e5419e07b8339821be6c71a8ae8727b3bd119b
farookie007 <faroukadekunle001@gmail.com>
2022-10-30 16:55:08 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the fizzbuzz program
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		/* checks if i is divisible by 3 or 5 */
+		if(!(i % 3) || !(i % 5))
+		{
+			if (!(i % 3))
+				printf("Fizz");
+			if (!(i % 5))
+				printf("Buzz");
+		} else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+}
