1b841b9801c099674454e9007288b5c0d2ab3ea6
farookie007 <faroukadekunle001@gmail.com>
2022-10-30 16:03:06 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,16 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	for
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar(\n);
+	}
+	if (size <= 0)
+		_putchar('\n');
 }
