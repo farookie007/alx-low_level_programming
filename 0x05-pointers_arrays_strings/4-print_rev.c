a6d4942ad095a2f3ab27d48b1756c8479d0f855b
farookie007 <faroukadekunle001@gmail.com>
2022-10-30 09:49:10 +0100

    Initial commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,49 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
- * print_rev - prints a string in reverse followed by a neewline
- * @s: the string to be printed
- *
- * Return: void
- */
-void print_rev(char *s)
-{
-	rev_string(s);
-}
-
-/**
- * rev_string - reverses a string and returns it
+ * print_rev - reverses a string and returns it
  * @s: the string to be reversed
  *
  * Return: char
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int len = _strlen(s);
-	char *arr = malloc((len + 1) * sizeof(char));
-	int i;
+	int i, j, end;
+	char str[800];
 
-	for (i = 0; i <= (len - 1); i++)
+	strcpy(str, s);
+
+	end = strlen(s) - 1;
+	i = 0;
+	j = end;
+	for (; i <= end; i++, j--)
 	{
-		arr[i] = *(s - i);
+		s[i] = str[j];
 	}
-	arr[len] = '\0';
-	printf("%s\n", arr);
-}
 
-/**
- * _strlen - Returns the length of the string argument
- * @s: pointer to the string address
- *
- * Return: the length as an integer
- */
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (s[count])
-		count++;
-	return (count);
+	printf("%s\n", s);
 }
