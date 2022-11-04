1b15f54850d02cecec9325779e3f7f9afb7b5cbe
farookie007 <faroukadekunle001@gmail.com>
2022-11-04 08:50:11 +0000

    Initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a newline
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+	}
+	else
+	{
+		printf("%c", *(s++));
+		_puts_recursion(s);
+	}
+}
