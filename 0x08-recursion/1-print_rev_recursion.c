1bd11a6fe333a7c435c6f8c1162da00c7c306556
farookie007 <faroukadekunle001@gmail.com>
2022-11-04 10:11:21 +0000

    Initial commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,54 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-
-void _puts_recursion(char *s);
-
+#include "main.h"
 
 /**
- * _rev - reverses a string
- * @s: the string to be reversed
- *
- * Return: void
- */
-void _rev(char *s)
-{
-	char str[500];
-	int j = 0;
-	int i = strlen(s) - 1;
-
-	strcpy(str, s);
-
-	for (; s[i]; i--, j++)
-		*(s + j) = str[i];
-}
-
-/**
- * _print_rev_recursion - prints a string in reverse recursively
- * @s: the string to be printed
- *
- * Return: void
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
 void _print_rev_recursion(char *s)
 {
-	_rev(s);
-	_puts_recursion(s);
-}
-
-/**
- * _puts_recursion - prints a string followed by a newline
- * @s: the string to be printed
- *
- * Return: void
- */
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
+	if (*s)
 	{
-		printf("\n");
-	}
-	else
-	{
-		printf("%c", *(s++));
-		_puts_recursion(s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
