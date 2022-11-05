99a987479e89b91acc799a74d1aa07b26d35e2d8
farookie007 <faroukadekunle001@gmail.com>
2022-11-05 21:47:58 +0000

    Initial commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,7 +25,9 @@ int _prime(int n, int i)
 {
 	if (n == 2 || n == 3)
 		return (1);
-	if ((n % i == 0) || (i > n / 2))
+	if (n % i == 0)
 		return (0);
+	if (i > n / 2)
+		return (1)
 	return (_prime(n, i + 1));
 }
