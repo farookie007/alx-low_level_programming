7cf567dff6c0b5fdf48ad033f71fc3a09930fbf0
farookie007 <faroukadekunle001@gmail.com>
2022-11-05 08:14:14 +0000

    Initial commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return _sqrt(n, 0);
+	return (_sqrt(n, 0));
 }
 
 /**
@@ -24,10 +24,10 @@ int _sqrt(int n, int i)
 {
 	/* confirms that the factor is less than half of the number */
 	/* also confirms that the square of the factor is less than the number */
-	if ((i > n / 2) || (i * i > n))
+	if ((i > n / 2 && i < n) || (i * i > n))
 		return (-1);
 	/* returns the factor if it's the square root */
 	if ((i * i) == n)
 		return (i);
-	return _sqrt(n, ++i);
+	return (_sqrt(n, ++i));
 }
