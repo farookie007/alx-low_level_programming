b4bf657eda0540f8c3ae42887de315c954b1e203
farookie007 <faroukadekunle001@gmail.com>
2022-11-05 07:49:51 +0100

    Initial commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - evaluates the factorial of a number
+ * @n: the number whose factorial is to be calculated
+ *
+ * Return: the calculated factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	else
+		return(n * factorial(n - 1);
+}
