81c1eba3fcee2af70a125fac3bad356b2fd5a543
farookie007 <faroukadekunle001@gmail.com>
2022-11-05 08:37:46 +0000

    Initial commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number
+ *
+ * Return: 1 if the number is a prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return _prime(n, 2);
+}
+
+/**
+ * _prime - checks for a prime number
+ * @n: the number
+ * @i: the factor
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int _prime(int n, int i)
+{
+	if (n == 2 || n == 3)
+		return (1);
+	if ((n % i == 0) || (i > n / 2))
+		return (0);
+	return _prime(n, ++i);
+}
