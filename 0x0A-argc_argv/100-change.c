31e4afc2a8b41a694240bc2dd2d482a3e479cebd
farookie007 <faroukadekunle001@gmail.com>
2022-11-08 08:01:26 +0000

    Initial commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - writes
+ * main - prints the minimum number of coins to make change for an amount
+ * @argc: the command line arguments count
+ * @argv: the array of command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
