29ae496fba578775b4e4810866ae0f26a9570c4a
farookie007 <faroukadekunle001@gmail.com>
2022-11-08 06:36:04 +0000

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program to prints its name
+ * @argc: the number of command line arguments
+ * @argv: the array of command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char* argv)
+{
+	printf("%s\n", argv[0]);
+}
