92c8f3af5fed27a3c09e5bc26a86421a7636ed57
farookie007 <faroukadekunle001@gmail.com>
2022-11-09 01:05:51 +0000

    Initial commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 1 if the strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+}
