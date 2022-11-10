70603b44f9870a02a34dd95c656cc2ec12a61515
farookie007 <faroukadekunle001@gmail.com>
2022-11-10 11:32:12 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer number
+ * @n: the number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	n = n
+	print_number(
+}
+
+
+void
