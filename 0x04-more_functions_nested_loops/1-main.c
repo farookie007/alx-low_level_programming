21f00e35e4d205242a5fef9d7a223c340e5763c2
farookie007 <faroukadekunle001@gmail.com>
2022-11-13 19:53:19 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", INT_MIN);
+	print_number(INT_MIN);
+	_putchar('\n');
+	return (0);
+}
