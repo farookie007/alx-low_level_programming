9fea845814361a9e3135f05be2e2c98b32648e99
farookie007 <faroukadekunle001@gmail.com>
2022-11-14 10:24:56 +0100

    Initial commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,28 +1,27 @@
+#include <string.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
  * *_strdup - copies the string given as parameter
+ * o a duplicate syring and returns rhe duplicate
  * @str: string to duplicate
  *
- * Return: pointer to the copied string (Success), NULL (Error)
+ * Return: pointer to the duplicate string (Success), NULL (Error)
  */
 char *_strdup(char *str)
 {
 	char *dup;
 	unsigned int i, len;
 
-	i = 0;
-	len = 0;
+	len = strlen(str);
 
 	if (str == NULL)
 		return (NULL);
 
-	while (str[len])
-		len++;
-
 	dup = malloc(sizeof(char) * (len + 1));
 
+	/* if malloc failed */
 	if (dup == NULL)
 		return (NULL);
 
