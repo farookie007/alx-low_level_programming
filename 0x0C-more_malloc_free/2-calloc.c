cf923fec49f4ad5ea60575193c7af1fdf849804c
farookie007 <faroukadekunle001@gmail.com>
2022-11-17 12:36:20 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	/* fills the array with zeroes */
-	for (i = 0; i < (int) nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		*(ptr + i) = 0;
 
 	return (ptr);
