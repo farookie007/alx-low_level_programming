6535bbe73bc2332dc729ecd6f11578bf8e2ff979
farookie007 <faroukadekunle001@gmail.com>
2022-11-17 19:17:13 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "main.h"
 
+
+char *_memset(char *s, char b, unsigned int n);
+
 /**
  * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
@@ -22,8 +25,23 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	/* fills the array with zeroes */
-	for (i = 0; i < nmemb * size; i++)
-		*(ptr + i) = 0;
+	_memset(ptr, '0', nmemb * size)
 
 	return (ptr);
+}
+/**
+ * *_memset - fills the memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to fill the memory with
+ * @n: number of memory space to fill
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	/* replacing the bytes from n-1 index with b */
+	while (n > 0)
+		s[--n] = b;
+
+	return (s);
 }
