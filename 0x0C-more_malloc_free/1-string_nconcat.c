deabb94319acbab1e441409a6bb125fc26580d8d
farookie007 <faroukadekunle001@gmail.com>
2022-11-17 11:39:44 +0100

    Initial commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,13 +18,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = s1 ? strlen(s1) : 0;
 	len2 = s2 ? strlen(s2) : 0;
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
-
 	for (i = 0; s1 && i < len1; i++)
 		s[i] = s1[i];
-
-	for (j = 0; s2 && j < len2; j++, i++)
-		s[i] = s2[j]
-
+  
+	for (j = 0; s2 && j < len2 && j <= n; j++, i++)
+		s[i] = s2[j];
 	s[i] = '\0';
 
 	return (s);
