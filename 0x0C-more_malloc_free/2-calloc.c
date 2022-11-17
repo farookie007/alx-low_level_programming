2bcc4d96b6d1a86a843895d0e88aa74a4cd4e11d
farookie007 <faroukadekunle001@gmail.com>
2022-11-17 19:22:21 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,25 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-char *_memset(char *s, char b, unsigned int n);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 
 /**
  * *_calloc - allocates memory for an array
@@ -15,32 +32,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
-	/* if nmemb or size equals to 0 */
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	/* if malloc fails */
-	ptr = (char *) malloc(nmemb * size);
-	if (!ptr)
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
 		return (NULL);
 
-	/* fills the array with zeroes */
-	_memset(ptr, '0', nmemb);
+	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
-}
-/**
- * *_memset - fills the memory with a constant byte
- * @s: memory area to be filled
- * @b: char to fill the memory with
- * @n: number of memory space to fill
- *
- * Return: pointer to s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	/* replacing the bytes from n-1 index with b */
-	while (n > 0)
-		s[--n] = b;
-
-	return (s);
 }
