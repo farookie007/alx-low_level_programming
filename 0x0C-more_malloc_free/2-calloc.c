5e6db615b0c65405750b91a1608d6c6c7a5a9478
farookie007 <faroukadekunle001@gmail.com>
2022-11-17 12:25:12 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,26 +2,6 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
-}
-
-/**
  * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
@@ -31,16 +11,19 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
+	int i;
 
+	/* if nmemb or size equals to 0 */
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
+	/* if malloc fails */
+	ptr = (char *) malloc(nmemb * size);
+	if (!ptr)
 		return (NULL);
 
-	_memset(ptr, 0, nmemb * size);
+	/* fills the array with zeroes */
+	for (i = 0; i < (int) nmemb; i++)
+		*(ptr + i) = 0;
 
 	return (ptr);
 }
