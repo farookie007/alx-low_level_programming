9b78f28afc47e591965796468b01bb2ea76ef292
farookie007 <faroukadekunle001@gmail.com>
2022-11-28 08:23:45 +0000

    Added comments for the file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
+ * @name: the name to be printed
+ * @f: the pointer to the function that prints the name
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	/* if the function or name is undefined */
+	if (!f || !name)
 		return;
+	/* calling the function */
 	f(name);
 }
