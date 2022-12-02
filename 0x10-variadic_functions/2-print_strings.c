f38ca767470be2ee37a5336bd871ec08fb766ab8
farookie007 <faroukadekunle001@gmail.com>
2022-12-02 12:07:47 +0000

    Initial commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,15 +16,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(list, char *);
+		va_arg(list, char *);
+
+		/* if str is NULL */
 		if (!str)
 			str = "(nil)";
-		if (!separator)
-			printf("%s", str);
-		else if (separator && i == 0)
-			printf("%s", str);
-		else
-			printf("%s%s", separator, str);
+		printf("%s", str);
+		/* if separator is pressent and it is the last element */
+		if (separator && i != (n-1))
+			printf("%s", separator);
 	}
 
 	printf("\n");
