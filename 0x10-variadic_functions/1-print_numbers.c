02f78ac883537485ac4496f5150ea608bbd57344
farookie007 <faroukadekunle001@gmail.com>
2022-12-02 10:35:20 +0000

    Betty: Added spaces around the '-' operator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args_list, int));
 
 		/* checks if there's a separator and not the last argument */
-		if (separator && (i != n-1))
+		if (separator && (i != n - 1))
 			printf("%s", separator);
 	}
 
