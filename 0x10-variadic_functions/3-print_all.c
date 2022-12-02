5de570c7683183c21a057e83ff3373835e7c002b
farookie007 <faroukadekunle001@gmail.com>
2022-12-02 12:17:57 +0000

    Resolved compilation error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
-					printf("%s", sep, str);
+					printf("%s", str);
 					break;
 				default:
 					i++;
