8dd381fb29bb8bb435e46e47fe37feedbc716e57
farookie007 <faroukadekunle001@gmail.com>
2022-12-03 13:01:34 +0000

    Initial commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
 
 	new = malloc(sizeof(list_t));
+	/* if malloc fails */
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+	new->len = strlen(str);
 	new->next = (*head);
 	(*head) = new;
 
