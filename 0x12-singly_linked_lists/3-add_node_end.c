a9566e62ef50f1bf418cee5935633c7f7d73e7ce
farookie007 <faroukadekunle001@gmail.com>
2022-12-03 13:10:49 +0000

    Initial commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,17 +13,13 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+	new->len = strlen(str);
 	new->next = NULL;
 
 	if (*head == NULL)
