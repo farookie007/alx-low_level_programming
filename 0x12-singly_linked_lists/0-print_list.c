3065a7ceb502f26db3c4bbc7487a9ad884b01060
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2022-12-30 23:22:53 +0000

    Resolved issue with betty coding style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to be printed
- * 
+ *
  * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
