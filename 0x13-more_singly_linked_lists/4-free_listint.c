452f8c22d6f77055979cce6a2d6f98b7a27c83f8
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2022-12-31 12:15:15 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,9 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	if (!head)
+		return;
+	if (head->next)
+		free_list(head->next);
+	free(head);
 }
