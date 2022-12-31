a20e72dcc4df96518d6120ec15d13f563a7bdf0a
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2022-12-31 12:55:44 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int num;
+	int data;
 
 	if (!head || !*head)
 		return (0);
 
-	num = (*head)->n;
 	temp = (*head)->next;
+	data = (*head)->n;
+
 	free(*head);
 	*head = temp;
 
-	return (num);
+	return (data);
 }
