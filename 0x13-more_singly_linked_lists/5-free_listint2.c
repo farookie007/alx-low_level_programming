925228f9f2082540450dc9112c3c3671f30fafc6
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2022-12-31 12:20:07 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,23 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp = *head;
 
-	if (head == NULL)
-		return;
+	free_listint(temp);
 
-	while (*head)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
-
+	/* assigning NULL to the new head */
 	*head = NULL;
+}
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	if (head->next)
+		free_listint(head->next);
+	free(head);
 }
