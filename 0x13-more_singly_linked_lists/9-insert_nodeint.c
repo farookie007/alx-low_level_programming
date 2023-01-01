22ac485ee8375b197e4e25f86e458cd93cbe3d32
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2023-01-01 06:05:32 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new;
 
-	new = (listint_t *)malloc(listint_t *);
+	new = (listint_t *)malloc(sizeof(listint_t *));
 	/* if malloc fails */
 	if (!new)
 		return (NULL);
