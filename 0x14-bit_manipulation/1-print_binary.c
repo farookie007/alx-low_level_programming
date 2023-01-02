97e1f8dbcd717b516f0b787aacc5bb129f608ef0
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2023-01-02 14:44:47 +0000

    Initial commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,6 +8,7 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int temp = n;
+	int shifts;
 
 	/* if n == 0 */
 	if (n == 0)
@@ -17,7 +18,7 @@ void print_binary(unsigned long int n)
 	}
 
 	/* counting the number of shifts to be made */
-	for (shifts = 0; (temp >>= 1) | 0; shift++)
+	for (shifts = 0; (temp >>= 1) | 0; shifts++)
 		;
 
 	/* shifting through n at the known number of times */
